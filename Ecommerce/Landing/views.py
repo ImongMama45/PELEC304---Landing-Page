162e963a47928f71e00d32f806eaea617f777949
ImongMama45 <quertrussellalisan@gmail.com>
2025-09-21 00:00:12 +0800

    Add files via upload

--- Ecommerce/Landing/views.py
+++ Ecommerce/Landing/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Items_1, Items_2
 
 # Create your views here.
 
 def Landing(request):
-    return render(request, 'Landing/home.html')
+    products = Items_1.objects.all()
+    formal = Items_2.objects.all()
+    context = {'products': products, 'formal' : formal}
+    return render(request, 'Landing/home.html', context)
